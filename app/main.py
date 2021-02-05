42c073a5700ab7d4fb67b84c29f54676b5e1bef2
Francisco Vicenzi <cchenzi@users.noreply.github.com>
2021-02-05 12:39:16 -0300

    Add window to add a new polygon

--- app/main.py
+++ app/main.py
@@ -2,10 +2,14 @@ import sys
 from main_window import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
 
-print(sys.argv)
-app = QApplication(sys.argv)
-window = QMainWindow()
-main_window = Ui_MainWindow()
-main_window.setupUi(window)
-window.show()
-sys.exit(app.exec_())
+def main():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    main_window = Ui_MainWindow()
+    main_window.setupUi(window)
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
