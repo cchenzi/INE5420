96ef848cb0ef39bec6e9098eecedc52e06a4bb22
kundlatsch <gustavo.kundlatsch@gmail.com>
2021-02-09 17:44:01 -0300

    Add color picker

--- app/wireframe.py
+++ app/wireframe.py
@@ -2,8 +2,9 @@ from utils import Shape
 
 
 class Wireframe:
-    def __init__(self, coordinates, index):
+    def __init__(self, coordinates, index, color):
         self.coordinates = coordinates
         self.number_points = len(self.coordinates)
         self.polygon_type = Shape(self.number_points).name
         self.name = f"{self.polygon_type}_{index}"
+        self.color = color
