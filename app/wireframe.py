677c8513eaae7ec475110bb543968c1b60914c63
Francisco Vicenzi <cchenzi@users.noreply.github.com>
2021-02-18 14:37:01 -0300

    Add functions to build transformation matrices

--- app/wireframe.py
+++ app/wireframe.py
@@ -1,3 +1,6 @@
+import numpy as np
+from functools import reduce
+
 from utils import Shape
 
 
@@ -5,6 +8,19 @@ class Wireframe:
     def __init__(self, coordinates, index, color):
         self.coordinates = coordinates
         self.number_points = len(self.coordinates)
+        ones = np.ones((self.number_points, 1))
+        self.homogeneous_coordinates = np.hstack((self.coordinates, ones))
         self.polygon_type = Shape(self.number_points).name
         self.name = f"{self.polygon_type}_{index}"
         self.color = color
+        self.transformations = []
+        self.transformed_coordinates = []
+        self.apply_transformations_to_points()
+
+    def apply_transformations_to_points(self):
+        transformed_points = []
+        for point in self.homogeneous_coordinates:
+            transformed = reduce(np.dot, [point, *self.transformations])
+            transformed_points.append((transformed[0], transformed[1]))
+
+        self.transformed_coordinates = transformed_points
